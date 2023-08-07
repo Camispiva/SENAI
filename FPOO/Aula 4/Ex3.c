25eaf9bcac1678d78f29aaa952092bebc9384d4e
camispiva <mila.piva.silva@gmail.com>
2023-08-07 11:14:24 -0300

    Aula 4

---
+++ FPOO/Aula 4/Ex3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	float a, b;
+	printf("Digite o salário:");
+	scanf("%f",&a);
+	printf("Digite o numero de filhos:");
+	scanf("%f",&b);
+	if(preco < 2000){
+		preco = preco * 45;
+	}
+	printf("O valor final é");
+	return 0;
+
+}
