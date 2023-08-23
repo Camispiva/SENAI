0b969f96fcdc5a9451cc900c2854db951938b366
camispiva <mila.piva.silva@gmail.com>
2023-08-23 10:42:41 -0300

    tarefinha

---
+++ FPOO/Exercicios da Aula 4/Ex5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	float preço, nome;
+	printf("Digite o nome do produto:");
+	scanf("%s",&nome);
+	printf("Digite o valor do produto:");
+	scanf("%f",&preco);
+	return 0;
+}
