ba5c29e2bbdb55af3f8858ae15b732bffa519f35
Camispiva <mila.piva.silva@gmail.com>
2023-10-03 11:13:25 -0300

    03/10

---
+++ LIMA/Aula 9/script.js
@@ -0,0 +1,146 @@
+// let - variável local 
+// var - variável global
+// const - variável constante
+
+// const obj = [
+//     {
+//         name: 'Lucas',
+//         age: 20,
+//         country: 'Brazl'
+//     },
+//     {
+//         name: 'Pedro',
+//         age: 40,
+//         country: 'Brazl'
+//     },
+//     {
+//         name: 'Julio',
+//         age: 30,
+//         country: 'Brazl'
+//     }
+// ]
+
+// console.log(obj);
+// console.warn('AVISO');
+// console.error('ERRO');
+// console.table(obj);// 
+
+// Operadores aritimeticos
+
+// + soma 
+// - subtração
+// * multiplicação
+// / divisão 
+// ** potenciação 
+// % resto da divisão 
+
+// Operadores de comparação 
+
+// > maior que 
+// < menor que
+// >= maior ou igual que 
+// <= menor ou igual que 
+// = atribuição 
+// == igualdade (valor)
+// != diferente (valor)
+// === igauladade (valor e tipo)
+// !== diferente (valor e tipo)
+
+// var a = 10;
+// var b = 15;
+// var c = 30; 
+
+// console.log((a + b) / c);
+
+
+// var vel = 100;
+// var dist = 25;
+// var temp = dist / vel;
+
+// console.log(temp);
+
+
+// var nome = 'Lucas';
+// var salario = 1000;
+// var reajuste = 10;
+// var novoSalario = salario + (salario * reajuste / 100);
+
+// console.log(nome + ' - novo salario: ' + novoSalario);
+
+// var data = new Date();
+
+// var dia = data.getDate();
+// var mes = data.getMonth() + 1;
+// var ano = data.getFullYear();
+
+// var anoCompleto = dia + '/' + mes + '/' + ano;
+// var anoCompleto = `${dia}/${mes}/${ano}`;
+
+// var hora = data.getHours();
+// var min = data.getMinutes();
+
+// var horaCompleta = `${hora}:${min}`;
+
+// console.log(anoCompleto, horaCompleta);
+
+// Math.ceil() - arredonda para cima
+// Math.floor() - arredonda para baixo
+// Math.round() - arredonda para o mais proximo 
+
+// var n = '10'
+
+// Number.parseInt(); - converte para inteiro 
+// number.parseFloat(); - converte para Float 
+
+// console.log(n, Number.parseFloat(n));
+
+// if(){
+
+// }else if(){
+
+// }
+
+// condição ? true : false; - if ternario
+
+// switch(){
+//     case 1:
+//         break;
+//     default:
+//         break;
+// }
+
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+// while(){
+
+// }
+
+// do{
+
+// }while()
+
+// var lista = 'a b c d e'.split(' ');
+
+// for(let i = 0; i < lista.length; i++){
+//     console.log(lista[i]);
+// }
+
+// lista.forEach(item => {
+//     console.log(item);
+// })
+
+// for(let item of lista){
+//     console.log(item);
+// }
+
+function nome (){
+
+}
+
+nome();
+
+
+
+
