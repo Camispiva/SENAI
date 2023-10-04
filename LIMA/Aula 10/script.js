c3015ec3dd59609559bee421febf2253eb74a12a
Camispiva <mila.piva.silva@gmail.com>
2023-10-04 11:01:18 -0300

    04/10

---
+++ LIMA/Aula 10/script.js
@@ -0,0 +1,18 @@
+// var nome = document.getElementById('nome')
+// var resultado = document.getElementById('resultado')
+
+// function buscarElemento(){
+//   //  let nome = document.getElementById('nome').value
+  
+//     resultado.innerHTML = nome.value
+// }                                    
+
+function calculo(){
+    let a = Number(document.getElementById('a').value);
+    let b = Number(document.getElementById('b').value);
+    let c = Number(document.getElementById('c').value);
+
+    document.querySelector('#result').innerHTML = `A soma de 'a' + 'b' dividido por 'c' é igual a: ${(a+b)/c}`
+
+
+}
